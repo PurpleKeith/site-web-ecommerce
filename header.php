f190e6fa91454d405d6aba47c833036e5543d6ce
PurpleKeith <79316424+PurpleKeith@users.noreply.github.com>
2021-02-19 15:22:14 +0100

    changement chemin absolue en chemin relatif

--- header.php
+++ header.php
@@ -21,25 +21,25 @@
             <div class="container">
                 <div class="navbar">
                     <div class="logo">
-                        <a href="../site d'E-commerce/index.php"><img src="images/logo.png" width="125px"></a>
+                        <a href="../index.php"><img src="images/logo.png" width="125px"></a>
                     </div>
                     <nav>
                         <ul id="MenuItems">
-                            <li><a href="../site d'E-commerce/index.php">Home</a></li>
-                            <li><a href="../site d'E-commerce/products.php">Products</a></li>
+                            <li><a href="index.php">Home</a></li>
+                            <li><a href="products.php">Products</a></li>
                             <li><a href="">About</a></li>
                             <li><a href="">Contact</a></li>
                             <?php
                                 if (isset($_SESSION["useruid"])) {
-                                    echo "<li><a href=\"../site d'E-commerce/profil.php\">Profil</a></li>";
+                                    echo "<li><a href=\"profil.php\">Profil</a></li>";
                                 }
                                 else {
-                                    echo "<li><a href=\"../site d'E-commerce/account.php\">Account</a></li>";
+                                    echo "<li><a href=\"account.php\">Account</a></li>";
                                 }
                             ?>
                         </ul>
                     </nav>
-                    <a href="../site d'E-commerce/cart.php"><img src="images/cart.png" width="30px" height="30px"></a>
+                    <a href="cart.php"><img src="images/cart.png" width="30px" height="30px"></a>
                     <img src="images/menu.png" class="menu-icon" onclick="menutoggle()"></a>
                 </div>
             </div>
